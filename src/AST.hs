7978c7c7d68c0e164d9151d1ac9d5c775e798fd4
Martin Ceresa <martinceresa@gmail.com>
2022-09-16 17:05:06 +0200

    First Parser and AST based on grammar in termina-bnd.md

--- src/AST.hs
+++ src/AST.hs
@@ -4,14 +4,77 @@ module AST where
 
 
 -- | Annotated AST
-data AAST a
-  = AST { glbs :: ([Global], a)
-        , fs  :: ([Function], a)
-        , ags :: ([Agent], a)
-    }
+data AASTElem a
+  -- | A task takes an `Identifier`, at least one parameter, a type and its body
+  = Task Identifier Param (CompoundStmt a) a
+  | Proc Identifier (Param, [Param]) BType (CompoundStmt a) a
+  | Handler Identifier [Param] (CompoundStmt a) a
+  | GlbDec Global a
 
-type Global = String
-type Function = String
-type Agent = String
+-- | Identifiers as `String`
+type Identifier = String
+-- | Addresses as `String`
+type Address = Integer
 
-type AST = AAST ()
+-- | Basic Types
+data BType
+  = UInt8 | UInt16 | UInt32 | UInt64
+  | Int8 | Int16 | Int32 | Int64
+  | Bool | Char
+
+----------------------------------------
+-- | Datatype representing Global Declarations
+data Global
+  = Atom BType Identifier
+  | Volatile BType Identifier Address
+  | MsgQueue BType Int Identifier
+  | Pool BType Int Identifier
+
+----------------------------------------
+newtype Param = Param { unParam :: (Identifier, BType)}
+paramIdent :: Param -> Identifier
+paramIdent = fst . unParam
+paramBType :: Param -> BType
+paramBType = snd . unParam
+
+data CompoundStmt a = Compound
+  {localDecl :: [LocalDecl a]
+  , stms :: [Stmt a]
+  }
+
+newtype LocalDecl a = LDecl {unLDecl :: (Identifier, BType, Const , a)}
+
+data Const = B Bool | I Int | C Char
+
+-- | Simple datatype enumerating some of the constructions of our language
+data Stmt a
+  = Assign
+  | Conditional a
+  | ForLoop a
+  | Skip a
+
+type AnnProgram a = [AASTElem a]
+
+-- When annotations are just `()` we get a normal ASTs and Programs
+type AST = AASTElem ()
+type Program = AnnProgram ()
+
+----------------------------------------
+-- Extra Info
+
+-- | Raw Data-types, Basic - Raw = {Bool, Char} declared as synonyms.
+data RawType
+  = RawUInt8 | RawUInt16 | RawUInt32 | RawUInt64
+  | RawInt8 | RawInt16 | RawInt32 | RawInt64
+
+basicToRaw :: BType -> RawType
+basicToRaw Bool = RawInt8
+basicToRaw Char = RawInt8
+basicToRaw UInt8 = RawUInt8
+basicToRaw UInt16 = RawUInt16
+basicToRaw UInt32 = RawUInt32
+basicToRaw UInt64 = RawUInt64
+basicToRaw Int8 = RawInt8
+basicToRaw Int16 = RawInt16
+basicToRaw Int32 = RawInt32
+basicToRaw Int64 = RawInt64
--- src/Parsing.hs
+++ src/Parsing.hs
@@ -11,9 +11,10 @@ import Text.Parsec.String
 -- Importing tokenizer
 import qualified Text.Parsec.Token as Tok
 import qualified Text.Parsec.Language as Lang
+import qualified Text.Parsec.Token as Tok
 
 -- | This is what we will build in this section
-type PositionAST = AAST SourcePos
+type PositionAST = AASTElem SourcePos
 
 ----------------------------------------
 -- Lexer
@@ -22,7 +23,20 @@ type PositionAST = AAST SourcePos
 lexer :: Tok.TokenParser ()
 lexer = Tok.makeTokenParser langDef
   where
-    reservedOps = ["Global","Function","Agent"]
+    reservedNames =
+      -- Globals
+      ["Atomic","Volatile","MsgQueue","Pool"]
+      ++ -- Basic Types
+      ["u8","u16","u32","u64"
+      ,"i8","i16","i32","i64"
+      ,"bool","char"]
+      ++ -- Declarations
+      ["Task","Procedure","Handler", "at"]
+      ++ -- Stmt
+      ["let", "skip"]
+      ++ -- Constants
+      ["true","false"]
+
     langDef =
       Lang.emptyDef{ Tok.commentStart = "/*"
                    , Tok.commentEnd = "*/"
@@ -34,7 +48,8 @@ lexer = Tok.makeTokenParser langDef
                    , Tok.identLetter = alphaNum <|> char '_'
                    -- | Operators begin with
                    , Tok.opStart = oneOf ":,;=&|<=>+-*/."
-                   , Tok.reservedNames = reservedOps
+                   , Tok.reservedNames = reservedNames
+                   , Tok.reservedOpNames = [":","=",";",","]
                    -- | Is the language case sensitive? It should be
                    , Tok.caseSensitive = True
                    }
@@ -47,40 +62,185 @@ wspcs = Tok.whiteSpace lexer
 braces :: Parsec String () a -> Parsec String () a
 braces = Tok.braces lexer
 
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+angles :: Parser a -> Parser a
+angles = Tok.angles lexer
+
+comma :: Parser String
+comma = Tok.comma lexer
+
+semi :: Parser String
+semi = Tok.semi lexer
+
 reserved :: String -> Parsec String () ()
 reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parsec String () ()
+reservedOp = Tok.reservedOp lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+hexa :: Parser Integer
+hexa = Tok.hexadecimal lexer
+
+number :: Parser Int
+number = fromInteger <$> Tok.natural lexer
 
 ----------------------------------------
 -- Parser
 ----------------------------------------
 
+-- | Types
+basicTypesParse :: Parser BType
+basicTypesParse =
+  (reserved "u8" >> return UInt8)
+  <|> (reserved "u16" >> return UInt16)
+  <|> (reserved "u32" >> return UInt32)
+  <|> (reserved "u64" >> return UInt64)
+  <|> (reserved "i8" >> return Int8)
+  <|> (reserved "i16" >> return Int16)
+  <|> (reserved "i32" >> return Int32)
+  <|> (reserved "i64" >> return Int64)
+  <|> (reserved "bool" >> return Bool)
+  <|> (reserved "char" >> return Char)
+
 multLines :: Parser [String]
 multLines = sepBy (many (satisfy (\c -> (c /= '}') && (c /= '{')) )) newline
-
-globals :: Parser [Global]
-globals = reserved "Global" >> braces multLines
-
-functions :: Parser [Function]
-functions = reserved "Function" >> braces multLines
-
-agents :: Parser [Agent]
-agents = reserved "Agent" >> braces multLines
 
 withPos :: Parser a -> Parser (a, SourcePos)
 withPos p = do
   pos <- getPosition
   res <- p
   return (res,pos)
+--
 
--- This is an unrealistic (but extremely simple) sourcecode parser
-topLevel :: Parser PositionAST
-topLevel = do
-  glbs_pos <- withPos globals
-  wspcs
-  fns_pos <- withPos functions
-  wspcs
-  ags_pos <- withPos agents
-  return $ AST glbs_pos fns_pos ags_pos
+paramParser :: Parser Param
+paramParser = curry Param <$> identifier <*> basicTypesParse
+
+paramParse1 :: Parser (Param, [Param])
+paramParse1 = sepBy1 paramParser comma
+  >>= \case
+   (x : xs) -> return (x,xs)
+   _ -> parserFail "Expecting at least one argument"
+
+-- -- Task Definition
+
+-- <task-definition> ::= 'task' <identifier> '(' <input-parameter> ')' <compound-statement>
+taskParser :: Parser PositionAST
+taskParser = do
+  p <- getPosition
+  reserved "Task"
+  name <- identifier
+  param <- parens paramParser
+  body <- compoundParser
+  return $ Task name param body p
+
+handlerParser :: Parser PositionAST
+handlerParser = do
+  reserved "Handler"
+  p <- getPosition
+  name <- identifier
+  params <- parens (sepEndBy paramParser comma)
+  compound <- compoundParser
+  return $ Handler name params compound p
+
+procedureParser :: Parser PositionAST
+procedureParser = do
+  p <- getPosition
+  reserved "Procedure"
+  name <- identifier
+  params <- parens paramParse1
+  reservedOp ":"
+  btype <- basicTypesParse
+  body <- compoundParser
+  return $ Proc name params btype body p
+
+constantParser :: Parser Const
+constantParser = parseLitInt <|> parseLitBool <|> parseLitChar
+  where
+    parseLitInt = I . fromIntegral <$> number
+    parseLitBool = (reserved "true" >> return (B True)) <|> (reserved "false" >> return (B False))
+    parseLitChar = C <$> anyChar
+
+localDeclarations :: Parser (LocalDecl SourcePos)
+localDeclarations = do
+  p <- getPosition
+  reserved "let"
+  name <- identifier
+  reservedOp ":"
+  ty <- basicTypesParse
+  reservedOp "="
+  val <- constantParser
+  semi
+  return $ LDecl (name, ty, val, p )
+
+stmtParser :: Parser (Stmt SourcePos)
+stmtParser = do
+  p <- getPosition
+  reserved "skip"
+  semi
+  return $ Skip p
+
+compoundParser :: Parser (CompoundStmt SourcePos)
+compoundParser = braces $
+  Compound <$> (many localDeclarations) <*> (many stmtParser)
+
+atomDeclParser :: Parser Global
+atomDeclParser = do
+  reserved "Atomic"
+  ty <- angles basicTypesParse
+  nm <- identifier
+  semi
+  return $ Atom ty nm
+
+volatileDeclParser :: Parser Global
+volatileDeclParser = do
+  reserved "Volatile"
+  ty <- angles basicTypesParse
+  nm <- identifier
+  reserved "at"
+  -- From doc: Parses a non-negative whole number in the hexadecimal system.
+  -- https://hackage.haskell.org/package/parsec-3.1.15.1/docs/Text-Parsec-Token.html
+  addr <- hexa
+  semi
+  return $ Volatile ty nm addr
+
+pairTypeInt :: Parser (BType, Int)
+pairTypeInt = do
+      ty <- basicTypesParse
+      comma
+      gint <- number
+      return (ty,gint)
+
+msgQueueDeclParser :: Parser Global
+msgQueueDeclParser = do
+  reserved "MsgQueue"
+  (ty, gint) <- angles pairTypeInt
+  nm <- identifier
+  semi
+  return $ MsgQueue ty gint nm
+
+poolDeclParser :: Parser Global
+poolDeclParser = do
+  reserved "Pool"
+  (ty,gint) <- angles pairTypeInt
+  nm <- identifier
+  semi
+  return $ Pool ty gint nm
+
+glblDecl :: Parser Global
+glblDecl = poolDeclParser <|> msgQueueDeclParser
+  <|> atomDeclParser  <|> volatileDeclParser
+
+-- | Top Level parser
+topLevel :: Parser [AASTElem SourcePos]
+topLevel = many $
+  taskParser <|> handlerParser <|> procedureParser
+  <|> (getPosition >>= \p -> GlbDec <$> glblDecl <*> pure p )
+
 
 contents :: Parser a -> Parser a
 contents p = wspcs *> p <* eof
