eab8790a779b8e9ee51e05d0860045d98cdf8248
Martin Ceresa <martin.ceresa@imdea.org>
2023-10-19 16:49:53 +0200

    Commit prebranching

--- src/Modules/Typing.hs
+++ src/Modules/Typing.hs
@@ -10,10 +10,35 @@ import Modules.Modules
 import qualified AST.Parser as PAST
 import Parser.Parsing (Annotation)
 import qualified AST.Seman as SAST
+import Semantic.Monad (SemanticAnns)
+
+import Control.Monad.Fail
 
 import qualified Data.Map.Strict as M
 
-typeProject :: Monad m =>
-  M.Map ModuleName ([ModuleName], PAST.TerminaProgram Annotation)
-  -> m (M.Map ModuleName ([ModuleName], SAST.TerminaProgram Annotation))
-typeProject = undefined
+data ModuleAST a = MData
+  { moduleDeps :: [ModuleName]
+  , moduleAST :: a
+  }
+
+type ParserProject = M.Map ModuleName (ModuleAST (PAST.TerminaProgram Annotation))
+type SemanProject =  M.Map ModuleName (ModuleAST (SAST.TerminaProgram SemanticAnns))
+
+-- type TypeProject = Except MErrors
+
+typeProject :: MonadFail m
+  -- We have a project just parsed
+  => ParserProject
+  -- a list indicating the order in which they need to be loaded.
+  -> [ModuleName]
+  -> m SemanProject
+typeProject parserMap = flip typeProject' M.empty
+  where
+    typeProject' [] typedP = return typedP
+    typeProject' (m:ms) tP =
+      case M.lookup m parserMap of
+        Nothing -> fail ("Module ("++ show m ++") in list but not parsed")
+        Just mPData ->
+          let deps = moduleDeps mPData in
+          let parsedButNotTyped = moduleAST mPData in
+          undefined
